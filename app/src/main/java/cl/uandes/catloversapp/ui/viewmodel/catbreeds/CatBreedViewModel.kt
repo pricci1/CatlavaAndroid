c5531911248cef68dbc10b34e81daf9a9f672752
Maria Pia Sanchez <mapi@platan.us>
2022-05-04 20:02:12 -0400

    feat(cat-breed-viewmodel): get breeds from api

--- app/src/main/java/cl/uandes/catloversapp/ui/viewmodel/catbreeds/CatBreedViewModel.kt
+++ app/src/main/java/cl/uandes/catloversapp/ui/viewmodel/catbreeds/CatBreedViewModel.kt
@@ -1,22 +1,35 @@
 package cl.uandes.catloversapp.ui.viewmodel.catbreeds
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cl.uandes.catloversapp.data.api.CatApiService
+import cl.uandes.catloversapp.data.datasources.InMemoryDataSource
 import cl.uandes.catloversapp.data.model.CatBreed
 import cl.uandes.catloversapp.data.model.CatBreeds
+import cl.uandes.catloversapp.data.repository.CatLoversRepository
+import kotlinx.coroutines.launch
 
-class CatBreedViewModel: ViewModel() {
+class CatBreedViewModel(application: Application): AndroidViewModel(application) {
+
+  private val repository = CatLoversRepository(
+    InMemoryDataSource(), CatApiService()
+  )
 
   val breedTitle = MutableLiveData("Cat Breeds")
-  internal val breedsLiveData = MutableLiveData<ArrayList<CatBreed>>()
+  internal val breedsLiveData = MutableLiveData<List<CatBreed>>()
   internal val loadingLiveData = MutableLiveData(true)
 
   fun refresh() {
-    breedsLiveData.value = loadCatBreeds()
+    loadCatBreeds()
     loadingLiveData.value = false
   }
 
-  private fun loadCatBreeds() : ArrayList<CatBreed> {
-    return CatBreeds.getBreeds()
+  private fun loadCatBreeds() {
+    viewModelScope.launch {
+      breedsLiveData.postValue(repository.getCatBreeds())
+    }
   }
 }
